c9b2895d11bf959a0dab574c1b69b778d39a76c2
Jaakko <jiisala@gmail.com>
2022-10-09 22:36:56 +0300

    more bufixing to get heroku running

--- db.py
+++ db.py
@@ -2,5 +2,8 @@ from os import getenv
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+uri = getenv("DATABASE_URL")
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config["SQLALCHEMY_DATABASE_URI"] = uri 
 db = SQLAlchemy(app)
